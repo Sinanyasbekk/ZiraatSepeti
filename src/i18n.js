dd7eb565dff1fb1ba4aa123f26370f4216e8e762
Sinanprs <93772230+Sinanprs@users.noreply.github.com>
2024-09-10 17:10:49 +0300

    i18n
    LanguageSupport

--- src/i18n.js
+++ src/i18n.js
@@ -1,11 +1,9 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
 
-// Import translation files
-import translationEN from './locales/en/translation.json';
-import translationTR from './locales/tr/translation.json';
+import translationEN from "./locales/en/translation.json";
+import translationTR from "./locales/tr/translation.json";
 
-// Configuration
 const resources = {
   en: {
     translation: translationEN,
@@ -15,15 +13,13 @@ const resources = {
   },
 };
 
-i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
-  .init({
-    resources,
-    lng: 'en', // default language
-    keySeparator: false, // we do not use keys in form messages.welcome
-    interpolation: {
-      escapeValue: false, // react already safes from xss
-    },
-  });
+i18n.use(initReactI18next).init({
+  resources,
+  lng: "en",
+  keySeparator: false,
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 export default i18n;
